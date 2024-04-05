b85e05892fb3a45fda3c4ea9e53a5ebb97165351
WYWDYX <127530392+WYWDYX@users.noreply.github.com>
2024-04-05 20:33:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 function feedCat() {
     var catImage = document.getElementById('cat');
-    catImage.style.transform = 'scale(10)'; // 放大猫咪图像
+    catImage.style.transform = 'scale(10)';
     setTimeout(() => {
-        catImage.style.transform = 'scale(1)'; // 恢复正常大小
+        catImage.style.transform = 'scale(1)';
     }, 500);
-    // 在此可以添加更多喂食的交互逻辑
 }
 
 function petCat() {
     var catImage = document.getElementById('cat');
-    catImage.style.transform = 'rotate(-900deg)'; // 小幅度旋转猫咪图像
+    catImage.style.transform = 'rotate(-900deg)';
     setTimeout(() => {
-        catImage.style.transform = 'rotate(0deg)'; // 恢复不旋转
+        catImage.style.transform = 'rotate(0deg)';
     }, 300);
-    // 在此可以添加更多抚摸的交互逻辑
 }
